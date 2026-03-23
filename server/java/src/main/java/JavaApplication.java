a3c3d4d28132f52eaf9406e2006b2006d342b4ba
Piotr Szerłomski <pszerlomski@gmail.com>
2026-03-23 17:20:49 +0100

    felete previous struct

--- server/java/src/main/java/JavaApplication.java
+++ server/java/src/main/java/JavaApplication.java
@@ -1,4 +1,4 @@
-package pl._Time.java;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- server/java/src/main/java/ServletInitializer.java
+++ server/java/src/main/java/ServletInitializer.java
@@ -1,4 +1,4 @@
-package pl._Time.java;
+
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
