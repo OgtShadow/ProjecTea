05ce70aea05aab91209d96dcffe122ae93ad1f31
AtkerHub <boczarkacper@gmail.com>
2026-06-30 17:01:10 +0200

    Revert "Moduł Kanban, integracja z bazą mognodb"
    This reverts commit 780a68bfc58c1063d58b10f52332c36de9c1d65b.

--- server/java/src/main/java/app/repository/mongo/KanbanTaskRepository.java
+++
@@ -1,9 +0,0 @@
-package app.repository.mongo;
-
-import app.model.KanbanTask;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface KanbanTaskRepository extends MongoRepository<KanbanTask, String> {
-}
