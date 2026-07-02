e73bc06d940484e72976e275299f93fadea8d2db
AtkerHub <boczarkacper@gmail.com>
2026-07-02 19:10:56 +0200

    Zmiana adresu MongoDB

--- server/java/src/main/java/app/config/MongoConfig.java
+++ server/java/src/main/java/app/config/MongoConfig.java
@@ -4,11 +4,17 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    // Wciągamy adres bazy dynamicznie (z pliku properties lub z chmury Render)
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
     @Override
     protected String getDatabaseName() {
         return "projectea_nosql";
@@ -16,7 +22,7 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
 
     @Override
     public MongoClient mongoClient() {
-        ConnectionString connectionString = new ConnectionString("mongodb://admin:adminpassword@mongodb:27017/projectea_nosql?authSource=admin");
+        ConnectionString connectionString = new ConnectionString(mongoUri);
         
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
             .applyConnectionString(connectionString)
